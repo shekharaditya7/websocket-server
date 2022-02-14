f3a48a899470b4e6a72724783b43211d772ac27a
adityaShekhar <aditya.s@toppr.com>
2022-02-14 11:44:22 +0530

    added stream comment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const http = require("http");
+const fs = require("fs");
 const { NODE_ENV } = process.env;
 const port = 8080;
 
 const requestListener = function (req, res) {
+  //STREAMS : Instead of waiting until the file is fully read, we start streaming it to the HTTP client as soon
+  // as we have a chunk of data ready to be sent. ----->
+
+  // const stream = fs.createReadStream(__dirname + "/data.txt");
+  // stream.pipe(res);
+
   res.writeHead(200);
   res.end("Hello, World!");
 };
