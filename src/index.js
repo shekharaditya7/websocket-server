d977fd4b71d9ddf04e06d21bbe2e78088c7f6f13
adityaShekhar <aditya.s@toppr.com>
2022-02-21 23:32:41 +0530

    initial socket io setup

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
+const express = require("express");
+const app = express();
 const http = require("http");
-const fs = require("fs");
-const { NODE_ENV } = process.env;
-const port = 8080;
+const server = http.createServer(app);
 
-const requestListener = function (req, res) {
-  //STREAMS : Instead of waiting until the file is fully read, we start streaming it to the HTTP client as soon
-  // as we have a chunk of data ready to be sent. ----->
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-  // const stream = fs.createReadStream(__dirname + "/data.txt");
-  // stream.pipe(res);
-
-  res.writeHead(200);
-  res.end("Hello, World!");
-};
-
-const server = http.createServer(requestListener);
-server.listen(port, (err) => {
-  if (err) throw err;
-  console.log(`> Ready on localhost: ${port} - env ${NODE_ENV}`);
+server.listen(8080, () => {
+  console.log("listening on * : 8080");
 });
