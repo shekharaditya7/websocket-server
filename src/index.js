135f2583b9c2105c4db07637b06dc44dfee380f7
adityaShekhar <aditya.s@toppr.com>
2022-02-21 23:50:35 +0530

    websocket initial

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.send("Server is healthy");
+});
+
+var connections = {};
+
+io.on("connection", (socket) => {
+  connections[socket.id] = socket.id;
+  console.log(socket.id, "connected");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(8080, () => {
-  console.log("listening on * : 8080");
+  console.log("listening on *: 8080");
 });
