61de34ad6cad4905af65a80adf794c7d1ceb043c
Aditya Shekhar <aditya.shekhar9@byjus.com>
2023-02-18 16:31:24 +0530

    Added Rooms and Group Chat

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const server = app.listen(8080);
 
+const CLIENT_ORIGIN =
+  process.env.NODE_ENV === "production"
+    ? "https://this-is-me-74cbf.web.app"
+    : "http://localhost:3000";
+
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: CLIENT_ORIGIN,
     methods: ["GET", "POST"],
   },
 });
@@ -13,11 +18,33 @@ app.get("/", (req, res) => {
   res.send("Server is healthy");
 });
 
-var connections = {};
+const createMessage = ({ socketId, text, type }) => {
+  return JSON.stringify({ socketId, text, type });
+};
 
 io.on("connection", (socket) => {
-  connections[socket.id] = socket.id;
-  console.log(socket.id, "connected");
+  const roomId = socket.handshake.query.roomId;
+  if (roomId) {
+    socket.join(roomId);
+    socket.to(roomId).emit(
+      "messageFromServer",
+      createMessage({
+        socketId: socket.id,
+        text: "Someone joined the chat",
+        type: "server",
+      })
+    );
+  }
+  socket.on("messageFromClient", (msg) => {
+    socket.to(roomId).emit(
+      "messageFromServer",
+      createMessage({
+        socketId: socket.id,
+        text: msg,
+        type: "user",
+      })
+    );
+  });
   socket.on("disconnect", () => {
     console.log(socket.id, "user disconnected");
   });
